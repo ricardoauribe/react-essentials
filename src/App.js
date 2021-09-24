2f8b5a84cbd31e9f4a370f3cb48431d5f1141b9c
Ricardo Uribe <ricardoauribe@hotmail.com>
2021-09-24 12:40:32 -0500

    Clearing out keys issue

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 
 function Header(props) {
-  console.log(props)
   return (
     <header>
       <h1>{props.name}'s Kitchen</h1>
@@ -14,7 +13,9 @@ function Main(props){
     <section>
       <p>We serve the most {props.adjective} food around</p>
       <ul style={{textAlign: "left"}}>
-        {props.dishes.map((dish)=> <li>{dish}</li>)}
+        {props.dishes.map((dish)=> (
+          <li key={dish.id}>{dish.title}</li>
+        ))}
       </ul>
     </section>
   );
@@ -33,15 +34,18 @@ function Footer (props) {
 const dishes = [
   "Pizza",
   "Hamburger",
-  "Sushi"
+  "Sushi",
+  "Tacos"
 ];
+
+const dishObject = dishes.map((dish, i)=> ({id:i, title: dish}));
 
 
 function App() {
   return (
     <div className="App">
       <Header name="Ricardo" />
-      <Main adjective="amazing" dishes={dishes} />
+      <Main adjective="amazing" dishes={dishObject} />
       <Footer year={new Date().getFullYear()}/>
     </div>
   );
