f58c8fe6672c2827f6abd87e41848d2d4724a3d1
Ricardo Uribe <ricardoauribe@hotmail.com>
2021-09-24 12:31:07 -0500

    working with lists

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ function Main(props){
   return (
     <section>
       <p>We serve the most {props.adjective} food around</p>
+      <ul style={{textAlign: "left"}}>
+        {props.dishes.map((dish)=> <li>{dish}</li>)}
+      </ul>
     </section>
   );
 }
@@ -27,11 +30,18 @@ function Footer (props) {
   );
 }
 
+const dishes = [
+  "Pizza",
+  "Hamburger",
+  "Sushi"
+];
+
+
 function App() {
   return (
     <div className="App">
       <Header name="Ricardo" />
-      <Main adjective="amazing" />
+      <Main adjective="amazing" dishes={dishes} />
       <Footer year={new Date().getFullYear()}/>
     </div>
   );
