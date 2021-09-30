e877f32180be0ab67288c4d6c63af78f163672d3
Ricardo Uribe <ricardoauribe@hotmail.com>
2021-09-30 11:58:08 -0500

    First approach to useState hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React, {useState} from 'react';
 import restaurant from './restaurant.jpeg';
 
 function Header(props) {
@@ -54,12 +55,18 @@ function RegularComponent() {
 
 function App(props) {
 
+  const [emotion, setEmotion] = useState("happy");
+
   return (
     <div className="App">
       <Header name="Ricardo" />
       <Main adjective="amazing" dishes={dishObject} />
       <Footer year={new Date().getFullYear()}/>
       {props.authorized ? <SecretComponent/> : <RegularComponent/> }
+      <h1>Current emotion is {emotion}. </h1>
+      <button onClick={()=> setEmotion("happy")}>Happy</button>
+      <button onClick={()=> setEmotion("frustrated")}>Frustrate</button>
+      <button onClick={()=> setEmotion("enthusiastic")}>Enthuse</button>
     </div>
   );
 }
